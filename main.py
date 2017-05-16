9252202b1cd2b963d193e431e86c8f2e220df6d5
kokimame <kokimametani@gmail.com>
2017-05-17 08:36:06 +0900

    First from mac

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import pygame as pg
+
+from network import *
+
+def main():
+    pg.init()
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%i,%i" % (600, 200)
+    os.environ['SDL_VIDEO_CENTERED'] = '0'
+    screen = pg.display.set_mode((600, 500))
+    done = False
+    learned = False
+    clock = pg.time.Clock()
+
+    nn = Network(screen)
+
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+            if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                done = True
+
+        if not learned:
+            learned = nn.is_training_done()
+
+
+        screen.fill(pg.Color("white"))
+        nn.paint()
+
+        pg.display.flip()
+        clock.tick(60)
+
+    # nn.training_phase()
+
+if __name__ == "__main__":
+    main()
